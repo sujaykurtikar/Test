65b1536f545ccf56848a7775d9401e906c7f8db4
Sujay Kurtikar <sujaykurtikar2@gmail.com>
2024-08-31 10:51:43 +0530

    Add project files.

---
+++ Project/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<Test_Project.Controllers.ControlController.ITaskStateService, Test_Project.Controllers.ControlController.TaskStateService>();
+builder.Services.AddSingleton<PeriodicTaskService>(); // Register PeriodicTaskService
+builder.Services.AddSingleton<HistoricalDataFetcher>();
+// Register the background service
+builder.Services.AddHostedService<PeriodicTaskService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
